535f855640434fe4236cbc760f2430e595315b81
Navis Ryu <navis@apache.org>
2016-10-20 03:58:35 +0900

    Math expressional parameters for aggregator (#2783)
    * Supports expression-paramed aggregator (squashed and rebased on master) also includes math post aggregator (was #2820)
    
    * Addressed comments
    
    * addressed comments

--- processing/src/test/java/io/druid/query/aggregation/FilteredAggregatorTest.java
+++ processing/src/test/java/io/druid/query/aggregation/FilteredAggregatorTest.java
@@ -40,6 +40,7 @@ import io.druid.segment.ColumnSelectorFactory;
 import io.druid.segment.DimensionSelector;
 import io.druid.segment.FloatColumnSelector;
 import io.druid.segment.LongColumnSelector;
+import io.druid.segment.NumericColumnSelector;
 import io.druid.segment.ObjectColumnSelector;
 import io.druid.segment.column.ColumnCapabilities;
 import io.druid.segment.column.ColumnCapabilitiesImpl;
@@ -195,6 +196,12 @@ public class FilteredAggregatorTest
           caps.setHasBitmapIndexes(true);
         }
         return caps;
+      }
+
+      @Override
+      public NumericColumnSelector makeMathExpressionSelector(String expression)
+      {
+        throw new UnsupportedOperationException();
       }
     };
   }
