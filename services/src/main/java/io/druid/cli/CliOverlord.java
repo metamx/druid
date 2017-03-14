9f571aba2d7746858193a0f1bb359646bae75221
leventov <leventov@ya.ru>
2017-03-13 19:12:13 -0600

    Make a single base interface for WorkerBehaviorConfig and TwoCloudConfig: BaseWorkerBehaviorConfig, use a single path for configuring both WorkerBehaviorConfig and TwoCloudConfig, use TwoCloudWorkerSelectStrategy in RemoteTaskRunner, when TwoCloud is configured

--- services/src/main/java/io/druid/cli/CliOverlord.java
+++ services/src/main/java/io/druid/cli/CliOverlord.java
@@ -73,8 +73,7 @@ import io.druid.indexing.overlord.helpers.TaskLogAutoCleaner;
 import io.druid.indexing.overlord.helpers.TaskLogAutoCleanerConfig;
 import io.druid.indexing.overlord.http.OverlordRedirectInfo;
 import io.druid.indexing.overlord.http.OverlordResource;
-import io.druid.indexing.overlord.setup.TwoCloudConfig;
-import io.druid.indexing.overlord.setup.WorkerBehaviorConfig;
+import io.druid.indexing.overlord.setup.BaseWorkerBehaviorConfig;
 import io.druid.indexing.overlord.supervisor.SupervisorManager;
 import io.druid.indexing.overlord.supervisor.SupervisorResource;
 import io.druid.indexing.worker.config.WorkerConfig;
@@ -226,8 +225,7 @@ public class CliOverlord extends ServerRunnable
             biddy.addBinding(RemoteTaskRunnerFactory.TYPE_NAME).to(RemoteTaskRunnerFactory.class).in(LazySingleton.class);
             binder.bind(RemoteTaskRunnerFactory.class).in(LazySingleton.class);
 
-            JacksonConfigProvider.bind(binder, WorkerBehaviorConfig.CONFIG_KEY, WorkerBehaviorConfig.class, null);
-            JacksonConfigProvider.bind(binder, TwoCloudConfig.CONFIG_KEY, TwoCloudConfig.class, null);
+            JacksonConfigProvider.bind(binder, BaseWorkerBehaviorConfig.CONFIG_KEY, BaseWorkerBehaviorConfig.class, null);
           }
 
           private void configureAutoscale(Binder binder)
