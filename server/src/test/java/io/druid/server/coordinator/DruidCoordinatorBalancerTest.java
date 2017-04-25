164541a6f37c26901865a23a202523d65dd546f9
dmitry.golitsyn <golitsyn.dima@gmail.com>
2017-04-25 16:22:19 +0300

    Remove unused imports

--- server/src/test/java/io/druid/server/coordinator/DruidCoordinatorBalancerTest.java
+++ server/src/test/java/io/druid/server/coordinator/DruidCoordinatorBalancerTest.java
@@ -37,12 +37,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
