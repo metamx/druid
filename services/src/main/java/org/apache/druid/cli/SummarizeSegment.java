e38a115a2b77d3c1a4c86a077f31e8636472ff7f
egor-ryashin <egor.ryashin@metamarkets.com>
2019-01-28 00:19:16 +0300

    fixed dropped count

---
+++ services/src/main/java/org/apache/druid/cli/SummarizeSegment.java
@@ -0,0 +1,345 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.cli;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
+import com.google.inject.Binder;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.name.Names;
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+import org.apache.commons.collections4.map.LinkedMap;
+import org.apache.druid.guice.DruidProcessingModule;
+import org.apache.druid.guice.QueryRunnerFactoryModule;
+import org.apache.druid.guice.QueryableModule;
+import org.apache.druid.java.util.common.granularity.Granularities;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.java.util.common.guava.Sequences;
+import org.apache.druid.java.util.common.logger.Logger;
+import org.apache.druid.query.DruidProcessingConfig;
+import org.apache.druid.segment.BaseObjectColumnValueSelector;
+import org.apache.druid.segment.ColumnSelectorFactory;
+import org.apache.druid.segment.ColumnValueSelector;
+import org.apache.druid.segment.Cursor;
+import org.apache.druid.segment.IndexIO;
+import org.apache.druid.segment.QueryableIndex;
+import org.apache.druid.segment.QueryableIndexStorageAdapter;
+import org.apache.druid.segment.VirtualColumns;
+import org.apache.druid.segment.column.ColumnConfig;
+import org.apache.druid.segment.column.ColumnHolder;
+import org.apache.druid.segment.filter.Filters;
+import org.joda.time.chrono.ISOChronology;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+@Command(
+    name = "summarize-segment"
+)
+public class SummarizeSegment extends GuiceRunnable {
+  public SummarizeSegment()
+  {
+    super(log);
+  }
+
+  private static final Logger log = new Logger(DumpSegment.class);
+
+  @Option(
+      name = {"-d", "--directory"},
+      title = "directory",
+      description = "Directory containing segment data.",
+      required = true)
+  public String directory;
+
+  @Option(
+      name = {"-o", "--out"},
+      title = "file",
+      description = "File to write to, or omit to write to stdout."
+  )
+  public String outputFileName;
+
+  @Override
+  public void run()
+  {
+    final Injector injector = makeInjector();
+    final IndexIO indexIO = injector.getInstance(IndexIO.class);
+    try (final QueryableIndex index = indexIO.loadIndex(new File(directory))) {
+      List<String> metricNames = getMetricNames(index);
+      summarize(injector, index, metricNames);
+      log.info(metricNames.toString());
+    }
+    catch (Exception e) {
+      throw Throwables.propagate(e);
+    }
+  }
+
+  private void summarize(Injector injector, QueryableIndex index, List<String> metricNames)
+  {
+    final QueryableIndexStorageAdapter adapter = new QueryableIndexStorageAdapter(index);
+    final List<String> columnNames = new ArrayList<>(metricNames);
+
+    final Sequence<Cursor> cursors = adapter.makeCursors(
+        Filters.toFilter(null),
+        index.getDataInterval().withChronology(ISOChronology.getInstanceUTC()),
+        VirtualColumns.EMPTY,
+        Granularities.ALL,
+        false,
+        null
+    );
+    class MetricStat {
+      double max;
+      List<Double> values = new ArrayList<Double>();
+      int []
+
+      public void updateMax(double aDouble)
+      {
+        max = Math.max(aDouble, max);
+      }
+
+      Map<String, Integer> blockerCounts = new TreeMap<>();
+      public void countBlockers(ArrayList<String> blockers)
+      {
+        for (String blocker : blockers) {
+          Integer orDefault = blockerCounts.getOrDefault(blocker, 0);
+          blockerCounts.put(blocker, orDefault + 1);
+        }
+      }
+
+      public void clearMax()
+      {
+        max = 0;
+      }
+    }
+      class Summary
+      {
+        private final List<String> columnNames;
+        int count;
+        private LinkedMap<String, MetricStat> metricStatsByName = new LinkedHashMap<>();
+        private List<MetricStat> metricStatsByIndex = new ArrayList<>();
+        int dropCount;
+        int retainedCount;
+        public double getDropped() {
+          return ((double)dropCount) / count;
+        }
+
+        public double getRetained() {
+          return ((double)retainedCount) / count;
+        }
+
+        public Summary(List<String> columnNames) {
+          this.columnNames = columnNames;
+          for (String columnName : columnNames) {
+            MetricStat metricStat = new MetricStat();
+            metricStatsByIndex.add(metricStat);
+            metricStatsByName.put(columnName, metricStat);
+          }
+        }
+
+        private MetricStat getStat(int i)
+        {
+          return metricStatsByIndex.get(i);
+        }
+        private MetricStat getStat(String columnName)
+        {
+          return metricStatsByName.get(columnName);
+        }
+
+        List<double[]> collected = new ArrayList<double[]>();
+        public void collect(double[] doubles)
+        {
+            collected.add(doubles);
+          for (int i = 0; i < doubles.length; i++) {
+            double aDouble = doubles[i];
+            getStat(i).updateMax(aDouble);
+          }
+        }
+
+        public void summarizeCollected()
+        {
+          double[] ratios = new double[columnNames.size()];
+          ArrayList<String> blockers = new ArrayList<>();
+          ArrayList<String> dropped = new ArrayList<>();
+          for (int rowIndex = 0; rowIndex < collected.size(); rowIndex++) {
+            double[] doubles = collected.get(rowIndex);
+            for (int columnIndex = 0; columnIndex < doubles.length; columnIndex++) {
+              double aDouble = doubles[columnIndex];
+              ratios[columnIndex] = aDouble / getStat(columnIndex).max;
+            }
+            for (int j = 0; j < ratios.length; j++) {
+              String columnName = columnNames.get(j);
+              double ratio = ratios[j];
+              if (ratio < 0.01) {
+                dropped.add(columnName);
+              } else {
+                blockers.add(columnName);
+              }
+            }
+            if (blockers.isEmpty())
+              dropCount++;
+            else if (blockers.size() != dropped.size()) {
+              retainedCount++;
+              countBlockers(dropped, blockers);
+            }
+            blockers.clear();
+            dropped.clear();
+          }
+        }
+
+        private void countBlockers(ArrayList<String> dropped, ArrayList<String> blockers)
+        {
+          for (int i = 0; i < dropped.size(); i++) {
+            String s = dropped.get(i);
+            getStat(s).countBlockers(blockers);
+          }
+        }
+
+        public void resetCollected()
+        {
+          collected.clear();
+          for (int i = 0; i < metricStatsByIndex.size(); i++) {
+            metricStatsByIndex.get(i).clearMax();
+          }
+        }
+
+//        public Map<String, Map<String, Integer>> getRetainedStats()
+//        {
+//          return null;
+//        }
+      }
+    Summary summary = new Summary(columnNames);
+    Sequence<Object> map = Sequences.map(
+        cursors,
+        cursor -> {
+          ColumnSelectorFactory columnSelectorFactory = cursor.getColumnSelectorFactory();
+          final List<BaseObjectColumnValueSelector> selectors = columnNames
+              .stream()
+              .map(columnSelectorFactory::makeColumnValueSelector)
+              .collect(Collectors.toList());
+          ColumnValueSelector timeValueSelector = columnSelectorFactory.makeColumnValueSelector(ColumnHolder.TIME_COLUMN_NAME);
+
+
+          // foreach column find max (create array of ColumnStat objects)
+          // foreach row find metric value ratio to max
+          //    if < 1% for all metrics drop row and count dropped rows
+          //    else < 1% at least one metric, create/update MetricStat for each metric < 1% (count metric > 1% - blocking metrics)
+          // print percentage of dropped rows
+          // foreach MetricStat print retained ratio to all rows, with percentage for each blocking metric
+          long previous = timeValueSelector.getLong();
+          while (!cursor.isDone()) {
+            summary.count++;
+
+              // iterate and find max
+              // iterate and count dropped
+            long time = timeValueSelector.getLong();
+            if (previous != time) {
+              summary.summarizeCollected();
+              summary.resetCollected();
+              previous = time;
+            }
+            double[] doubles = new double[columnNames.size()];
+            for (int i = 0; i < columnNames.size(); i++) {
+              BaseObjectColumnValueSelector baseObjectColumnValueSelector = selectors.get(i);
+              Number value = (Number) baseObjectColumnValueSelector.getObject();
+              doubles[i] = value.doubleValue();
+            }
+            summary.collect(doubles);
+            cursor.advance();
+          }
+          return null;
+        }
+    );
+    map.accumulate(null, (accumulated, in) -> null);
+
+
+    System.out.println(String.format("Dropped %.3f Retained %.3f", summary.getDropped(), summary.getRetained()));
+    for (String columnName : columnNames) {
+//      summary.metricStatsByName.get(columnName)
+    }
+    System.out.println(String.format("Dropped %.3f Retained %.3f", summary.getDropped(), summary.getRetained()));
+
+
+  }
+
+  private List<String> getMetricNames(QueryableIndex index)
+  {
+    Set<String> columnNames = new LinkedHashSet<>(index.getColumnNames());
+    Set<String> availableDimensions = Sets.newLinkedHashSet(index.getAvailableDimensions());
+    return new ArrayList<>(Sets.difference(columnNames, availableDimensions));
+  }
+
+  @Override
+  protected List<? extends Module> getModules()
+  {
+    return ImmutableList.of(
+        new DruidProcessingModule(),
+        new QueryableModule(),
+        new QueryRunnerFactoryModule(),
+        new Module()
+        {
+          @Override
+          public void configure(Binder binder)
+          {
+            binder.bindConstant().annotatedWith(Names.named("serviceName")).to("druid/tool");
+            binder.bindConstant().annotatedWith(Names.named("servicePort")).to(9999);
+            binder.bindConstant().annotatedWith(Names.named("tlsServicePort")).to(-1);
+            binder.bind(DruidProcessingConfig.class).toInstance(
+                new DruidProcessingConfig()
+                {
+                  @Override
+                  public String getFormatString()
+                  {
+                    return "processing-%s";
+                  }
+
+                  @Override
+                  public int intermediateComputeSizeBytes()
+                  {
+                    return 100 * 1024 * 1024;
+                  }
+
+                  @Override
+                  public int getNumThreads()
+                  {
+                    return 1;
+                  }
+
+                  @Override
+                  public int columnCacheSizeBytes()
+                  {
+                    return 25 * 1024 * 1024;
+                  }
+                }
+            );
+            binder.bind(ColumnConfig.class).to(DruidProcessingConfig.class);
+          }
+        }
+    );
+  }
+}
